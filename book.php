f9f7f97d36b45166581bb452e05d659471e6b1c4
Andrus Naulainen <andrus.naulainen@gmail.com>
2020-09-14 09:57:05 +0300

    Single book view with data

--- book.php
+++ book.php
@@ -19,5 +19,40 @@ $book = $stmt->fetch();
 </head>
 <body>
     <h1><?php echo $book['title']; ?></h1>
+
+    <img src="<?php echo $book['cover_path']; ?>">
+
+    <table>
+        <tr>
+            <td>Aasta:</td>
+            <td><?php echo $book['release_date']; ?></td>
+        </tr>
+        <tr>
+            <td>Kirjeldus:</td>
+            <td><?php echo $book['summary']; ?></td>
+        </tr>
+        <tr>
+            <td>Hind:</td>
+            <td><?php echo str_replace('.', ',', round($book['price'], 2)); ?> &euro;</td>
+        </tr>
+        <tr>
+            <td>Lehekülgi:</td>
+            <td><?php echo $book['pages']; ?></td>
+        </tr>
+        <tr>
+            <td>Laos:</td>
+            <td><?php echo $book['stock_saldo'] > 0 ? 'Jah' : 'Ei'; ?></td>
+        </tr>
+        <tr>
+            <td>Tüüp:</td>
+            <td><?php echo $book['type'] == 'ebook' ? 'E-raamat' : $book['type'] == 'used' ? 'Kasutatud' : 'Uus'; ?></td>
+        </tr>
+    </table>
+
+    <div style="margin-top: 2em;">
+        <span><a href="editform.php">muuda</a></span>
+        <span><a href="delete.php">kustuta</a></span>
+    </div>
+
 </body>
 </html>
